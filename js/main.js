f879720a3d396254a0eac4e8816df2f5e09e0cb0
Anton Kim <kim.anton@gmail.com>
2023-01-26 18:29:16 -0800

    c

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-d3.select("body").append("p").text("Hello World!");
+const numericData = [1, 2, 4, 8, 16];
+
+// Add <svg> element (drawing space)
+const svg = d3.select('body').append('svg')
+    .attr('width', 300)
+    .attr('height', 50);
+
+// Add rectangle
+svg.selectAll('rect')
+    .data(numericData)
+    .enter()
+    .append('rect')
+    .attr('fill', 'red')
+    .attr('width', 50)
+    .attr('height', 50)
+    .attr('y', 0)
+    .attr('x', (d, index) => index * 60);
